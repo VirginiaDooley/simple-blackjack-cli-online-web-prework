03074169df6f25a6eb9a091d01b31ea79f6d6909
Virginia Dooley <vcdooley@gmail.com>
2018-09-21 18:06:36 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,6 +57,9 @@ def runner
 until card_total > 21
   card_total = hit?(card_total)
   display_card_total(card_total)
+  if card_total >= 16
+    break
+    puts "Congrats, you win with a score of #{card_total}. Thanks for playing!"
   end
  end_game(card_total)
 end
